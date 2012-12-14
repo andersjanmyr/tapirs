0fe07738de9fdab377dce9d62005761dfdff3a91
Anders Janmyr <anders@janmyr.com>
2012-12-14 11:40:48 +0100

    Added a favicon

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.use(corsMiddleware);
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
